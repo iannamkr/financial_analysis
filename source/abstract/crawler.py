3f33351a7fdd4966a14a071d1b76d6962f3da374
Ian nam <ian.nam.kr@gmail.com>
2020-07-09 00:00:51 +0900

    Add dividend & 3 year bond yield
    Add dividend & 3 year bond yield
    
    Add dividend & 3 year bond yield
    
    Add dividend & 3 year bond yield
    
    Add dividend & 3 year bond yield
    
    Add dividend & 3 year bond yield

--- source/abstract/crawler.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class AbstractCrawler(ABC):
-    @abstractmethod
-    def crawl(self, symbol, url, path):
-        raise NotImplementedError
--- source/portfolio/FundamentalAnalysisPortfolio.py
+++
@@ -1,8 +0,0 @@
-from abstract.portfolio import AbstractPortfolio
-
-
-# TODO
-class FundamentalAnalysisPortfolio(AbstractPortfolio):
-    def __init__(self, config, symbols):
-        pass
-
